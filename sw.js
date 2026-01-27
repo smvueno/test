28181b1420a3b345fb5e01c7094249f5fbc74917
smvueno <smvueno@users.noreply.github.com>
2026-01-27 02:29:34 +0000

    Deploying to gh-pages from @ smvueno/fruit-bounce-merge@4d76a4b3e2d798d5a06f3825621300541a3d84b8 🚀

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for offline support
-const CACHE_NAME = 'fruit-bounce-v1-' + '1769480192353'; // Will be replaced at build time
+const CACHE_NAME = 'fruit-bounce-v1-' + '1769480973862'; // Will be replaced at build time
 const urlsToCache = [
     './',
     './index.html'
